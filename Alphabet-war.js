cd9081a53efd115dff2c24465f20b4ce2b593bfc
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-25 11:25:01 -0800

    Update Alphabet-war.js

--- Alphabet-war.js
+++ Alphabet-war.js
@@ -41,3 +41,15 @@ Example:
 
 
 // Solution
+
+function alphabetWar(str) {
+  const arr = str.replace(/(?:.)?\*+.?/g, '').split('');
+  const [ left, right ] = arr
+    .reduce((acc,curr) => {
+      acc[0] += { w: 4, p: 3, b: 2, s: 1 }[curr] || 0;
+      acc[1] += { m: 4, q: 3, d: 2, z: 1 }[curr] || 0;
+      return acc;
+    }, [ 0, 0 ]);
+  
+  return left > right ? 'Left side wins!' : right > left ? 'Right side wins!' : 'Let\'s fight again!';
+}
